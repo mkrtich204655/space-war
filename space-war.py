e573a217fb0b1f580655f565b16edd18a9758f7d
Mkrtich Mkrtchyan <mkrtich@bl.ink>
2024-04-22 14:05:50 +0400

    part 3

--- space-war.py
+++ space-war.py
@@ -29,6 +29,19 @@ class Sprite(turtle.Turtle):
 
     def move(self):
         self.fd(self.speed)
+        if self.xcor() > 290:
+            self.setx(290)
+            self.rt(60)
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.rt(60)
+
+        if self.ycor() > 290:
+            self.sety(290)
+            self.rt(60)
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.rt(60)
 
 class Player(Sprite):
     def __init__(self, spriteshap, color, startx, starty):
@@ -47,6 +60,36 @@ class Player(Sprite):
 
     def decelerate(self):
         self.speed -= 1
+
+
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+
+    def draw_boarder(self):
+        # Boarders
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300, 300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600)
+            self.pen.rt(90)
+        self.pen.penup()
+        self.pen.ht()
+
+# Create Game
+
+game = Game()
+
+game.draw_boarder()
+
 
 
 
